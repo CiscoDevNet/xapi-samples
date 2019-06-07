0fd2fbeda62d20a798a4b8aa144fdd661a6d9b40
Stève Sfartz (ObjectIsAdvantag) <steve.sfartz@gmail.com>
2019-06-08 01:51:24 +0200

    enhancement hue requests

--- macros/11-philips-hue.js
+++ macros/11-philips-hue.js
@@ -1,62 +1,52 @@
 /**
- * Post messages to a Hue Bridge
+ * Post API requests to a Hue Bridge
  *
  */
 
-//
-// Philipps Hue Library
-//
+const xapi = require("xapi")
 
-function updateLight(bridgeip, username, light, payload, cb) {
-
-  // Post message
-  xapi.command(
-    'HttpClient Put',
-    {
-      Header: ["Content-Type: application/json"],
-      Url: `http://${bridgeip}/api/${username}/lights/${light}/state`,
-      AllowInsecureHTTPS: "True",
-      ResultBody: 'plaintext'
-    },
-    JSON.stringify(payload))
-    .then((response) => {
-      if (response.StatusCode == 200) {
-        console.log("message pushed to bridge")
-        
-        // Retrieve response
-        console.debug(`received response: ${response.Body}`)
-        let result = JSON.parse(response.Body)
-        if (result[0] && (result[0].success)) {
-          console.debug("success")
-          if (cb) cb(null, result[0].success)
-          return
-        }
-        
-        if (result[0] && (result[0].error)) {
-          console.debug("error")
-          if (cb) cb(null, result[0].error)
-          return
-        }
-        
-        return
-        
-      }
-            
-      console.log("failed with status code: " + response.StatusCode)
-      if (cb) cb("failed with status code: " + response.StatusCode, response.StatusCode)
-    })
-    .catch((err) => {
-      console.log("failed with err: " + err.message)
-      if (cb) cb("Could not contact the bridge")
-    })
-}
-
-
-// Your Philips Hue deployment
-const HUE_BRIDGE = '192.168.1.33'
-const HUE_USERNAME = 'SECRET'
+// Update for your Hue deployment
+const BRIDGE_IP = "192.168.1.33"
+const BRIDGE_USER = "SECRET"
+const LIGHT_ID = 1 // number of your Bulb as registered at your Hue Bridge
 
 // Change color
-const HUE_LIGHT = 4 // number of the light in your deployment
-const color = 25500 
-updateLight(HUE_BRIDGE, HUE_USERNAME, HUE_LIGHT, { "hue": color }, console.log)
+const color = 25500
+sendToBridge(BRIDGE_IP, BRIDGE_USER, LIGHT_ID, "Put", { "hue": color })
+
+
+function sendToBridge(bridgeip, username, light, verb, payload) {
+
+  xapi.command("HttpClient " + verb, {
+    Header: ["Content-Type: application/json"],
+    Url: `http://${bridgeip}/api/${username}/lights/${light}/state`,
+    AllowInsecureHTTPS: "True",
+    ResultBody: "plaintext"
+  },
+    JSON.stringify(payload))
+    .then((response) => {
+
+      console.log(`request sent to the bridge, status code: ${response.StatusCode}`)
+      console.debug(`received response: ${response.Body}`)
+
+      // Check response
+      let result = JSON.parse(response.Body)
+      if (result[0] && (result[0].success)) {
+        console.log("success")
+        return
+      }
+
+      if (result[0] && (result[0].error)) {
+        console.log("error")
+        console.debug(result[0].error.Message)
+        return
+      }
+
+      console.warn(`unexpected response: ${Object.keys(result[0])}`)
+      return
+    })
+    .catch((err) => {
+      console.log("Could not contact the bridge")
+      console.debug("failed with err: " + err.message)
+    })
+}
